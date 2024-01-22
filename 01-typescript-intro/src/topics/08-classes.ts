4be679805e64ff83ccf433f85ececa7baac96512
Michael Rondon <mrondon72@gmail.com>
2024-01-22 23:06:51 +0100

    27. Extender una clase

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -3,11 +3,21 @@ export class Person {
     // public name: string;
 
     constructor(
-        private address: string = 'No Address',
-        public name: string
+        public name: string,
+        private address: string = 'No Address'
     ) { }
 }
 
-const ironman = new Person('New York', 'Ironman');
+export class Hero extends Person {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string
+    ) {
+        super(realName, 'New York');
+    }
+}
+
+const ironman = new Hero('Ironman', 45, 'Tony Stark');
 
 console.log(ironman);
