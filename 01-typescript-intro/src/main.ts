e47538cbe8ad2bf36d1de63ec8a3f884951dbc04
Michael Rondon <mrondon72@gmail.com>
2024-01-22 16:39:26 +0100

    15. Tipos básicos y conceptos generales

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,8 +1,8 @@
-import './style.css'
+import './style.css';
+// import './topics/01-basic-types';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
   Hola Mundo
-`
-
+`;
