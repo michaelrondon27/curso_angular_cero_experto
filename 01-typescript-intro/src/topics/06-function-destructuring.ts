9c48ebb28dccef1b027fb6e7aa0b7fb4f0effb31
Michael Rondon <mrondon72@gmail.com>
2024-01-22 22:29:25 +0100

    22. Desestructuración de argumentos

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,39 @@
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Nokia A1',
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: 'iPad Air',
+    price: 250.0
+}
+
+interface TaxCalculationOptions {
+    products: Product[];
+    tax: number;
+}
+
+function taxCalculation(options: TaxCalculationOptions): number[] {
+    let total = 0;
+
+    options.products.forEach(product => {
+        total += product.price;
+    });
+
+    return [total, total * options.tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax: number = 0.15;
+
+const result: number[] = taxCalculation({products: shoppingCart, tax});
+
+console.log('Total', result[0]);
+console.log('Tax', result[1]);
+
+export {}
