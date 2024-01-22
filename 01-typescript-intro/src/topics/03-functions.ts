1c4882450136103a40438361e8ade20f29fa6474
Michael Rondon <mrondon72@gmail.com>
2024-01-22 17:40:32 +0100

    17. Funciones básicas

---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,17 @@
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+const addNumbersArrow = (a: number, b: number): string => `${a + b}`;
+
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2) {
+    return firstNumber * base;
+}
+
+const result: number = addNumbers(1, 2);
+const result2: string = addNumbersArrow(1, 2);
+const multipleResult: number = multiply(5);
+
+console.log({result, result2, multipleResult});
+
+export {}
