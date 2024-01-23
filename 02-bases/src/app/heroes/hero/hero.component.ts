84339b51cd66c17fe6dc67367258ce35b68a469a
Michael Rondon <mrondon72@gmail.com>
2024-01-23 16:06:51 +0100

    45. Interpolación, estructura HTML y estilos

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    public age: number = 45;
+    public name: string = 'ironman';
 
 }
