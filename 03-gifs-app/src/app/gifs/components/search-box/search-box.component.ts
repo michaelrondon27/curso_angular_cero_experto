50b5f5270314a2e51998ae782b95308d62f8d8ed
Michael Rondon <mrondon72@gmail.com>
2024-01-24 13:35:40 +0100

    89. Resolución de la tarea - CardList

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gifs-search-box',
-  template: `
-    <h5>Buscar:</h5>
+    selector: 'gifs-search-box',
+    template: `
+        <h5>Buscar:</h5>
 
-    <input
-        type="text"
-        class="form-control"
-        placeholder="Buscar gifs..."
-    >
-  `
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Buscar gifs..."
+        >
+    `
 })
 export class SearchBoxComponent {
 
