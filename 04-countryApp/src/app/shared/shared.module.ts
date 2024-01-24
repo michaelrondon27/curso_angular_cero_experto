304c4da63f2f4682374950e3b18a30c7eb8c7ea8
Michael Rondon <mrondon72@gmail.com>
2024-01-24 21:46:04 +0100

    116. RouterLink y RouterLinkoptions

--- 04-countryApp/src/app/shared/shared.module.ts
+++ 04-countryApp/src/app/shared/shared.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
     declarations: [
         AboutPageComponent,
-        HomePageComponent
+        HomePageComponent,
+        SidebarComponent
     ],
     exports: [
         AboutPageComponent,
-        HomePageComponent
+        HomePageComponent,
+        SidebarComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        RouterModule
     ]
 })
 export class SharedModule { }
