25c579a5cb1fdc05f859bf530cb4c4d53c9ad515
Michael Rondon <mrondon72@gmail.com>
2024-01-24 13:59:28 +0100

    91. GifsService

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../../services/gifs.service';
+
 @Component({
     selector: 'gifs-search-box',
     template: `
@@ -18,10 +20,16 @@ export class SearchBoxComponent {
 
     @ViewChild('txtTagInput') public tagInput!: ElementRef<HTMLInputElement>;
 
+    constructor(
+        private gifsService: GifsService
+    ) { }
+
     searchTag(): void {
         const newTag: string = this.tagInput.nativeElement.value;
 
-        console.log({ newTag })
+       this.gifsService.searchTag(newTag);
+
+       this.tagInput.nativeElement.value = '';
     }
 
 }
