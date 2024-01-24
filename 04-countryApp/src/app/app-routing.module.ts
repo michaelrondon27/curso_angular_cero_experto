3090e09132c77ab24340a43187a507afab88bb32
Michael Rondon <mrondon72@gmail.com>
2024-01-24 21:51:28 +0100

    117. Resolución de la tarea

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
     {
         path: 'about',
         component: AboutPageComponent
+    },
+    {
+        path: 'contact',
+        component: ContactPageComponent
     },
     {
         path: '**',
