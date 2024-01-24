b978b7b758a56ca8bba5d0416df76d1971c3586e
Michael Rondon <mrondon72@gmail.com>
2024-01-24 15:31:24 +0100

    100. Leer del LocalStorage

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -16,10 +16,22 @@ export class GifsService {
 
     constructor(
         private http: HttpClient
-    ) { }
+    ) {
+        this.loadLocalStorage();
+    }
 
     get tagsHistory(): string[] {
         return [...this._tagsHistory];
+    }
+
+    private loadLocalStorage(): void {
+        if (!localStorage.getItem('history')) return;
+
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+        if (this._tagsHistory.length === 0) return;
+
+        this.searchTag(this._tagsHistory[0]);
     }
 
     private organizeHistory(tag: string) {
