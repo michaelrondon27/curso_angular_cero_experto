7067fb47cda3980bed5f77c7a383aab33e3f95ab
Michael Rondon <mrondon72@gmail.com>
2024-01-24 16:12:29 +0100

    108. Lazy Image - Parte 2

--- 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html'
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+    public hasLoaded: boolean = false;
+
+    @Input() public alt: string = '';
+    @Input() public url!: string;
+
+    ngOnInit(): void {
+        if (!this.url) throw new Error('Url property is required');
+    }
+
+    onLoad() {
+        this.hasLoaded = true;
+    }
 
 }
