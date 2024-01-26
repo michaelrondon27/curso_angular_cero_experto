8d11b4290c7e2e8b789ac5d003d19a603c094196
Michael Rondon <mrondon72@gmail.com>
2024-01-26 14:36:52 +0100

    152. PrimeNG - Menú superior

---
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+    selector: 'shared-menu',
+    templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+    public menuItems: MenuItem[] = [];
+
+    ngOnInit() {
+        this.menuItems = [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            }
+        ];
+    }
+
+}
