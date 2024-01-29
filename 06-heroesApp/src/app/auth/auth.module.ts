07031d33e6b55354229f8f5a66eae47fc4abeea6
Michael Rondon <mrondon72@gmail.com>
2024-01-29 23:32:04 +0100

    192. Diseño de Login y Auth Layout

--- 06-heroesApp/src/app/auth/auth.module.ts
+++ 06-heroesApp/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -14,8 +15,9 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
         RegisterPageComponent
     ],
     imports: [
+        AuthRoutingModule,
         CommonModule,
-        AuthRoutingModule
+        MaterialModule
     ]
 })
 export class AuthModule { }
