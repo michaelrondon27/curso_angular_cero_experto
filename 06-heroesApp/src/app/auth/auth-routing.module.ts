a1ea646907e583521ab613f29e8547506ced7a08
Michael Rondon <mrondon72@gmail.com>
2024-01-29 21:34:09 +0100

    184. Ruta hijas y LazyLoad

--- 06-heroesApp/src/app/auth/auth-routing.module.ts
+++ 06-heroesApp/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { LayoutPageComponent } from "./pages/layout-page/layout-page.component";
+
+// auth/
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutPageComponent
+    }
+];
 
 @NgModule({
     exports: [
--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+
+// heroes/
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutPageComponent,
+        children: [
+            {
+                path: 'new-hero',
+                component: NewPageComponent
+            }
+        ]
+    }
+];
 
 @NgModule({
     exports: [
