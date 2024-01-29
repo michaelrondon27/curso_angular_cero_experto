ad22c3a2e90386a0db770d02877e12b730b5e504
Michael Rondon <mrondon72@gmail.com>
2024-01-29 13:52:27 +0100

    162. I18nSelectPipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-uncommon-page',
-  templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+    selector: 'app-uncommon-page',
+    templateUrl: './uncommon-page.component.html',
+    styleUrls: ['./uncommon-page.component.css']
 })
-export class UncommonPageComponent implements OnInit {
+export class UncommonPageComponent {
 
-  constructor() { }
+    // i18n Select
+    public gender: 'female'|'male' = 'male';
+    public invitationMap = {
+        female: 'invitarla',
+        male: 'invitarlo'
+    };
+    public name: string = 'Michael';
 
-  ngOnInit(): void {
-  }
+    changeClient(): void {
+        this.gender = 'female';
+        this.name = 'Melissa';
+    }
 
 }
