52b8c9ab26868a59267d192871daa70893bf7566
Michael Rondon <mrondon72@gmail.com>
2024-01-29 13:38:02 +0100

    160. Decimal, Currency y Percent Pipes

--- 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numbers-page',
-  templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.css']
+    selector: 'app-numbers-page',
+    templateUrl: './numbers-page.component.html',
+    styleUrls: ['./numbers-page.component.css']
 })
-export class NumbersPageComponent implements OnInit {
+export class NumbersPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    public percent: number = 0.4856;
+    public totalSells: number = 2567789.5567;
 
 }
