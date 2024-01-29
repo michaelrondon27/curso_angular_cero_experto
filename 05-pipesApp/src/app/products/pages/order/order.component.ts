f49552dcbaebe7011194fb42104f0f7828f41fe0
Michael Rondon <mrondon72@gmail.com>
2024-01-29 19:38:05 +0100

    171. ToggleCasePipe - Piper Personalizado

--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-order',
+    selector: 'products-order',
     templateUrl: './order.component.html'
 })
 export class OrderComponent {
--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { ProductsRoutingModule } from './products-routing.module';
 
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { OrderComponent } from './pages/order/order.component';
@@ -14,6 +16,7 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
         BasicsPageComponent,
         NumbersPageComponent,
         OrderComponent,
+        ToggleCasePipe,
         UncommonPageComponent
     ],
     imports: [
