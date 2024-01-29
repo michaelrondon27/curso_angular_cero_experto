efce4e254708fe2a14b07a8f7243e0e6a1254b0b
Michael Rondon <mrondon72@gmail.com>
2024-01-29 20:07:29 +0100

    174. PrimeNG - Table

---
+++ 05-pipesApp/src/app/products/interfaces/hero.interface.ts
@@ -0,0 +1,9 @@
+export enum Color {
+    red, black, blue, green
+}
+
+export interface Hero {
+    canFly: boolean;
+    color:  Color;
+    name:   string;
+}
--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 
+import { Color, Hero } from '../../interfaces/hero.interface';
+
 @Component({
     selector: 'products-order',
     templateUrl: './order.component.html'
 })
 export class OrderComponent {
 
+    public heroes: Hero[] = [
+        {
+            canFly: true,
+            color: Color.blue,
+            name: 'Superman'
+        },
+        {
+            canFly: false,
+            color: Color.black,
+            name: 'Batman'
+        },
+        {
+            canFly: false,
+            color: Color.red,
+            name: 'Daredevil'
+        },
+        {
+            canFly: false,
+            color: Color.red,
+            name: 'Robin'
+        },
+        {
+            canFly: true,
+            color: Color.green,
+            name: 'Linterna Verde'
+        }
+    ];
     public isUpperCase: boolean = false;
 
     toggleUpperCase(): void {
