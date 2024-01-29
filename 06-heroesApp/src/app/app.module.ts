5dc970e96437a7d8f2258a04d9a6607741491ff8
Michael Rondon <mrondon72@gmail.com>
2024-01-29 23:14:34 +0100

    191. Instalación de Angular Material y PrimeFlex

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         AppRoutingModule,
+        BrowserAnimationsModule,
         BrowserModule,
         SharedModule
     ],
