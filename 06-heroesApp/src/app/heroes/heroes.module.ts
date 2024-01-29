7e58c82dde8e361f663e7d8d5fdcc172b5d11a88
Michael Rondon <mrondon72@gmail.com>
2024-01-30 00:43:55 +0100

    197. Mostrar listado de Héroes

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
+import { CardComponent } from './components/card/card.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
@@ -12,6 +13,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
     declarations: [
+        CardComponent,
         HeroPageComponent,
         LayoutPageComponent,
         ListPageComponent,
