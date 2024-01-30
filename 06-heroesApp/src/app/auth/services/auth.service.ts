e68ac0937ed86076678127cebeafc1020dd88dc8
Michael Rondon <mrondon72@gmail.com>
2024-01-30 18:05:08 +0100

    222. AuthService - Servicio de Autenticación

---
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+
+import { environments } from '../../../environments/environment';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    private baseUrl: string = environments.baseUrl;
+    private user?: User;
+
+    constructor(
+        private httpClient: HttpClient
+    ) { }
+
+    get currentUser(): User | undefined {
+        if (!this.user) return undefined;
+
+        return structuredClone(this.user);
+    }
+
+    login(email: string, password: string): Observable<User> {
+        return this.httpClient.get<User>(`${ this.baseUrl }/users/1`)
+            .pipe(
+                tap(user => this.user = user),
+                tap(user => localStorage.setItem('token', user.id.toString()))
+            );
+    }
+
+}
