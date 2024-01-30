7d3a156dcf52b637f052e45e6f7a624ac43461e7
Michael Rondon <mrondon72@gmail.com>
2024-01-30 14:02:26 +0100

    202. Angular Material - Autocomplete Parte 2

--- 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
     selector: 'app-search-page',
@@ -7,6 +11,33 @@ import { FormControl } from '@angular/forms';
 })
 export class SearchPageComponent {
 
+    public heroes: Hero[] = [];
     public searchInput: FormControl = new FormControl('');
+    public selectedHero?: Hero;
+
+    constructor(
+        private heroesService: HeroesService
+    ) { }
+
+    onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+        if (!event.option.value) {
+            this.selectedHero = undefined;
+            return;
+        }
+
+        const hero: Hero = event.option.value;
+
+        this.searchInput.setValue(hero.superhero);
+
+        this.selectedHero = hero;
+        console.log('selectedHero', this.selectedHero);
+    }
+
+    searchHero(): void {
+        const value: string = this.searchInput.value || '';
+
+        this.heroesService.getSuggestions(value)
+            .subscribe(heroes => this.heroes = heroes);
+    }
 
 }
