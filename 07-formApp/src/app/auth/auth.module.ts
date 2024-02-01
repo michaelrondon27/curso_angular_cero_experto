08dd3d88aa224adba89a3e7aad3d7215a4f86504
Michael Rondon <mrondon72@gmail.com>
2024-02-01 21:22:31 +0100

    251. Formulario de Registro

--- 07-formApp/src/app/auth/auth.module.ts
+++ 07-formApp/src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -10,8 +11,9 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
         RegisterPageComponent
     ],
     imports: [
+        AuthRoutingModule,
         CommonModule,
-        AuthRoutingModule
+        ReactiveFormsModule
     ]
 })
 export class AuthModule { }
--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register-page',
-  templateUrl: './register-page.component.html',
-  styles: [
-  ]
+    selector: 'app-register-page',
+    templateUrl: './register-page.component.html'
 })
-export class RegisterPageComponent implements OnInit {
+export class RegisterPageComponent {
 
-  constructor() { }
+    public myForm: FormGroup = this.formBuilder.group({
+        email: ['', [Validators.required]],
+        name: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        password2: ['', [Validators.required]],
+        username: ['', [Validators.required]]
+    });
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
+
+    isValidField(field: string) {
+
+    }
+
+    onSubmit() {
+        this.myForm.markAllAsTouched();
+    }
 
 }
