f91b9491cd683cd2e9d69512db110f0ee67d7fe6
Michael Rondon <mrondon72@gmail.com>
2024-02-01 16:12:35 +0100

    242. Validaciones Dinámicas

--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -23,6 +23,32 @@ export class DynamicPageComponent {
         return this.myForm.get('favoriteGames') as FormArray;
     }
 
+    getFieldError(field: string): string | null {
+        if (!this.myForm.controls[field]) return null;
+
+        const errors = this.myForm.controls[field].errors || {};
+
+        for (const key of Object.keys(errors)) {
+            switch (key) {
+                case 'required':
+                    return 'Este campo es requerido';
+
+                    case 'minlength':
+                        return `Mínimo ${ errors['minlength'].requiredLength } caracteres.`;
+            }
+        }
+
+        return null;
+    }
+
+    isValidField(field: string): boolean | null {
+        return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+    }
+
+    isValidFieldInArray(formArray: FormArray, index: number): boolean | null {
+        return formArray.controls[index].errors && formArray.controls[index].touched;
+    }
+
     onSubmit(): void {
         if (this.myForm.invalid) {
             this.myForm.markAllAsTouched();
