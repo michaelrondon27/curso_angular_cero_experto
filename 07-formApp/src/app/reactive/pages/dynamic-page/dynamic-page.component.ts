3d0d805e23a25258c1b5c6213cd405ed65af5bfc
Michael Rondon <mrondon72@gmail.com>
2024-02-01 16:06:43 +0100

    241. Formularios dinámicos

--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-dynamic-page',
-  templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+    selector: 'app-dynamic-page',
+    templateUrl: './dynamic-page.component.html'
 })
-export class DynamicPageComponent implements OnInit {
+export class DynamicPageComponent {
 
-  constructor() { }
+    public myForm: FormGroup = this.formBuilder.group({
+        favoriteGames: this.formBuilder.array([
+            ['Death Stranding', Validators.required],
+            ['Metal Gear', Validators.required]
+        ]),
+        name: ['', [Validators.required, Validators.minLength(3)]]
+    });
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
+
+    get favoriteGames(): FormArray {
+        return this.myForm.get('favoriteGames') as FormArray;
+    }
+
+    onSubmit(): void {
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched();
+            return;
+        }
+
+        this.myForm.reset();
+    }
 
 }
