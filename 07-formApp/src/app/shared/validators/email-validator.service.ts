7aa2f3fc31838bd23536841fa1f0dbd1050b4703
Michael Rondon <mrondon72@gmail.com>
2024-02-01 22:32:41 +0100

    256. Elaborar el validador de correo

--- 07-formApp/src/app/shared/validators/email-validator.service.ts
+++ 07-formApp/src/app/shared/validators/email-validator.service.ts
@@ -7,14 +7,34 @@ import { Observable, delay, of } from 'rxjs';
 })
 export class EmailValidatorService implements AsyncValidator {
 
+    // validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
+    //     const email = control.value;
+
+    //     return of({
+    //         emailTaken: true
+    //     }).pipe(
+    //         delay(2000)
+    //     );
+    // }
+
     validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
         const email = control.value;
 
-        return of({
-            emailTaken: true
+        const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+            if (email === 'mrondon@gmail.com') {
+                subscriber.next({
+                    emailTaken: true
+                });
+                subscriber.complete();
+            }
+
+            subscriber.next(null);
+            subscriber.complete();
         }).pipe(
-            delay(2000)
+            delay(3000)
         );
+
+        return httpCallObservable;
     }
 
 }
