ba2cdb2c6fd2bc0252ffa914d59ecbf4deced691
Michael Rondon <mrondon72@gmail.com>
2024-02-02 14:25:11 +0100

    263. Formulario reactivo - primer selector

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-selector-page',
     templateUrl: './selector-page.component.html'
 })
 export class SelectorPageComponent {
+
+    public myForm: FormGroup = this.formBuilder.group({
+        borders: ['', Validators.required],
+        country: ['', Validators.required],
+        region:  ['', Validators.required]
+    });
+
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
 
 }
