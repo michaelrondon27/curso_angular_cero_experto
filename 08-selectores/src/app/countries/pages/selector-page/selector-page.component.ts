daac6a8f2c4b462fec66614a568738954b007184
Michael Rondon <mrondon72@gmail.com>
2024-02-02 15:21:36 +0100

    270. Limpiar país cuando el primer selector cambia

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
 import { CountriesService } from '../../services/countries.service';
@@ -34,6 +34,7 @@ export class SelectorPageComponent implements OnInit {
     onRegionChanged(): void {
         this.myForm.get('region')!.valueChanges
             .pipe(
+                tap(() => this.myForm.get('country')!.setValue('')),
                 switchMap(region => this.countriesService.getCountriesByRegion(region))
             )
             .subscribe(countries => this.countriesByRegion = countries);
