b8f392e4c8910f10132fdca3760b97953ca71518
Michael Rondon <mrondon72@gmail.com>
2024-02-02 17:49:55 +0100

    276. Inicio de proyecto - LifeCycle

---
+++ 09-lifecycle/src/app/products/pages/product/product.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'products-product-page',
+    templateUrl: './product.component.html'
+})
+export class ProductComponent {
+
+}
