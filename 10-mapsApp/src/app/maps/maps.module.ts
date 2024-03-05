206cda10a1205ebd40cd6146ca17564d72d85344
Michael Rondon <mrondon72@gmail.com>
2024-03-05 01:37:45 +0100

    287. Crear los components y rutas

---
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+
+// Layout
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+
+// Pages
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+
+// Routes
+import { MapsRoutingModule } from './maps-routing.module';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+@NgModule({
+    declarations: [
+        FullScreenPageComponent,
+        MapsLayoutComponent,
+        MiniMapComponent,
+        MarkersPageComponent,
+        PropertiesPageComponent,
+        SideMenuComponent,
+        ZoomRangePageComponent
+    ],
+    imports: [
+        CommonModule,
+        MapsRoutingModule
+    ]
+})
+export class MapsModule { }
