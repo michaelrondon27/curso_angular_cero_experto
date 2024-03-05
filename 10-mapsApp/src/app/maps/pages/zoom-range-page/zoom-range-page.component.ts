a57ff069cca8c1dd83cf39e5c6a392575e5b7608
Michael Rondon <mrondon72@gmail.com>
2024-03-05 21:54:06 +0100

    294. Controlar el nivel del Zoom

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,11 +1,14 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
+import { Map } from 'mapbox-gl';
 
 @Component({
     templateUrl: './zoom-range-page.component.html',
     styleUrl: './zoom-range-page.component.css'
 })
 export class ZoomRangePageComponent  implements AfterViewInit {
+
+    public map?: Map;
+    public zoom: number = 10;
 
     @ViewChild('map') public divMap?: ElementRef;
 
@@ -14,12 +17,46 @@ export class ZoomRangePageComponent  implements AfterViewInit {
             throw 'El elemento HTML no fue encontrado';
         }
 
-        const map = new mapboxgl.Map({
+        this.map = new Map({
             center: [-74.5, 40],
             container: this.divMap.nativeElement,
             style: 'mapbox://styles/mapbox/streets-v12',
-            zoom: 9
+            zoom: this.zoom
         });
+
+        this.mapListeners();
+    }
+
+    mapListeners(): void {
+        if (!this.map) {
+            throw 'Mapa no inicializado';
+        }
+
+        this.map.on('zoom', (ev) => {
+            this.zoom = this.map!.getZoom();
+        });
+
+        this.map.on('zoomend', (ev) => {
+            if (this.map!.getZoom() < 18) {
+                return;
+            }
+
+            this.map!.zoomTo(18);
+        });
+    }
+
+    zoomChanged(value: string): void {
+        this.zoom = Number(value);
+
+        this.map?.zoomTo(this.zoom);
+    }
+
+    zoomIn(): void {
+        this.map?.zoomIn();
+    }
+
+    zoomOut(): void {
+        this.map?.zoomOut();
     }
 
 }
