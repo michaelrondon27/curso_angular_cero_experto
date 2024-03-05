4e86b901f93daf44c812f6d8c1880592ea423c82
Michael Rondon <mrondon72@gmail.com>
2024-03-05 22:19:47 +0100

    296. Marcadores en el mapa

--- 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
-  templateUrl: './markers-page.component.html',
-  styleUrl: './markers-page.component.css'
+    templateUrl: './markers-page.component.html',
+    styleUrl: './markers-page.component.css'
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+
+    public currentLngLat: LngLat = new LngLat(-74.5, 40);
+    public map?: Map;
+
+    @ViewChild('map') public divMap?: ElementRef;
+
+    ngAfterViewInit(): void {
+        if (!this.divMap) {
+            throw 'El elemento HTML no fue encontrado';
+        }
+
+        this.map = new Map({
+            center: this.currentLngLat,
+            container: this.divMap.nativeElement,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            zoom: 10
+        });
+    }
 
 }
