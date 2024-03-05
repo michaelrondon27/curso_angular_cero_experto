f13a7e1d8f51e84bd8c9fc3c4c313e8b23a598f0
Michael Rondon <mrondon72@gmail.com>
2024-03-05 21:39:36 +0100

    293. Diseño - ZoomIn y ZoomOut

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
-  templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.css'
+    templateUrl: './zoom-range-page.component.html',
+    styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent  implements AfterViewInit {
+
+    @ViewChild('map') public divMap?: ElementRef;
+
+    ngAfterViewInit(): void {
+        if (!this.divMap) {
+            throw 'El elemento HTML no fue encontrado';
+        }
+
+        const map = new mapboxgl.Map({
+            center: [-74.5, 40],
+            container: this.divMap.nativeElement,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            zoom: 9
+        });
+    }
 
 }
