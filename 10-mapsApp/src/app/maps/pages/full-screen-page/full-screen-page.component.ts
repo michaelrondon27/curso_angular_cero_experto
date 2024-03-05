592bb92f2f52f71f4f281bd60f62d7b2dec6ea08
Michael Rondon <mrondon72@gmail.com>
2024-03-05 21:18:03 +0100

    291. Mostrar un mapa en pantalla completa

--- 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoibXJvbmRvbjcyIiwiYSI6ImNqeGJyNnVhYzA3MWwzeXBlaHlzc3ZleG4ifQ.TwvVQCK3WtQFEVKGdEp1kg';
 
 @Component({
-  templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+    templateUrl: './full-screen-page.component.html',
+    styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+    ngAfterViewInit(): void {
+        const map = new mapboxgl.Map({
+            center: [-74.5, 40],
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v12',
+            zoom: 9
+        });
+    }
 
 }
