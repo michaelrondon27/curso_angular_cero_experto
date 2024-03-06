188fdfcf677ca508f963ab3f80c47b44f75167fe
Michael Rondon <mrondon72@gmail.com>
2024-03-06 13:11:24 +0100

    320. Directive Input - Cambiar el color dle host

--- 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
+++ 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
@@ -1,11 +1,17 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
     selector: '[customLabel]'
 })
 export class CustomLabelDirective implements OnInit {
 
+    private _color: string = 'red';
     private htmlElement?: ElementRef<HTMLElement>;
+
+    @Input() set color(value: string) {
+        this._color = value;
+        this.setStyle();
+    }
 
     constructor(
         private elementRef: ElementRef<HTMLElement>
@@ -14,7 +20,15 @@ export class CustomLabelDirective implements OnInit {
     }
 
     ngOnInit(): void {
+        this.setStyle();
+    }
 
+    setStyle(): void {
+        if (!this.htmlElement) {
+            return;
+        }
+
+        this.htmlElement.nativeElement.style.color = this._color;
     }
 
 }
