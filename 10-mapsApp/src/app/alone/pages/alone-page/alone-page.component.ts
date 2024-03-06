8ee70b2b093064c16b3b3acfaf9efd2458403020
Michael Rondon <mrondon72@gmail.com>
2024-03-06 11:49:00 +0100

    309. Standalone - Como componente reutilizable

--- 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
+++ 10-mapsApp/src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,11 +1,15 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+// Components
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+
 @Component({
-  standalone: true,
-//   imports: [CommonModule],
-  templateUrl: './alone-page.component.html',
-  styleUrl: './alone-page.component.css'
+    standalone: true,
+    imports: [
+        CounterAloneComponent
+    ],
+    templateUrl: './alone-page.component.html',
+    styleUrl: './alone-page.component.css'
 })
 export class AlonePageComponent {
 
