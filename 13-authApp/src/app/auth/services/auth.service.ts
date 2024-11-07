8e2fab4c9d4aefe2481ddaaf41562be604bd5ca0
Michael Rondon <mrondon72@gmail.com>
2024-11-07 02:21:41 +0100

    363. AuthService

--- 13-authApp/src/app/auth/services/auth.service.ts
+++ 13-authApp/src/app/auth/services/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, WritableSignal, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+// Environments
+import { environment } from '@env/environments';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+    private httpClient: HttpClient = inject(HttpClient);
+
+    private readonly baseUrl: string = environment.baseUrl;
+
+    private _authStatus : WritableSignal<AuthStatus> = signal<AuthStatus>();
+    private _currentUser: WritableSignal<User> = signal<User>(null);
+
+    constructor() { }
+
+    login(email: string, password: string): Observable<boolean> {
+        return of(true);
+    }
+
 }
