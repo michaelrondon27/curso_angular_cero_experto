7a898d442b64cf4f0de83fbc1c925523e29fb847
Michael Rondon <mrondon72@gmail.com>
2024-11-11 00:53:14 +0100

    Control Flow - @If

--- 14-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ 14-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 
 @Component({
     standalone: true,
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
     styles: ``
 })
 export default class ControlFlowComponent {
+
+    public showContent: WritableSignal<boolean> = signal<boolean>(false);
+
+    toggleContent(): void {
+        this.showContent.update(value => !value);
+    }
 
 }
