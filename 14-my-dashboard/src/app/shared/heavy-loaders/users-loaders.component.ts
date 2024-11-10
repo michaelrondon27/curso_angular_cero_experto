66bd196022dd81127dab7a6f17f97ef86ccd152f
Michael Rondon <mrondon72@gmail.com>
2024-11-10 23:48:03 +0100

    392. Creación de archivos y directorios

---
+++ 14-my-dashboard/src/app/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-users-loaders',
+    standalone: true,
+    imports: [],
+    template: `
+        <h1>Hola Mundo</h1>
+    `
+})
+export class UsersLoadersComponent {
+
+}
