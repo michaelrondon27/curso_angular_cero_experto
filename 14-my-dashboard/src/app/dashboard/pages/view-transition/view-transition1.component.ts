14e84b01b43d375027768f685859e4a2a3e846b0
Michael Rondon <mrondon72@gmail.com>
2024-11-11 13:50:28 +0100

    406. ViewTransitions API

---
+++ 14-my-dashboard/src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+// Components
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+    standalone: true,
+    imports: [
+        TitleComponent
+    ],
+    template: `
+        <app-title title="View Transition 1" />
+
+        <section class="flex justify-start">
+            <img
+                alt="Picsum"
+                height="300"
+                srcset="https://picsum.photos/id/237/200/300"
+                width="200"
+            />
+
+            <div class="bg-blue-500 w-56 h-56">
+
+            </div>
+        </section>
+    `,
+    styles: ``
+})
+export default class ViewTransition1Component {
+
+}
