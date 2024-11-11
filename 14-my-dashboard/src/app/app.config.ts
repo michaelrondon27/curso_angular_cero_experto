7590f3532f7b42fbd57c65553fa34e8afdd01549
Michael Rondon <mrondon72@gmail.com>
2024-11-11 18:35:14 +0100

    415. Actualización de un proyecto de Angular

--- 14-my-dashboard/src/app/app.config.ts
+++ 14-my-dashboard/src/app/app.config.ts
@@ -1,14 +1,12 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        importProvidersFrom(
-            HttpClientModule
-        ),
+        provideHttpClient(withInterceptorsFromDi()),
         provideRouter(
             routes,
             withViewTransitions()
