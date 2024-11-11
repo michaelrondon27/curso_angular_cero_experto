9423c22a29b5dc54a38734b67b603b7e4401b6ad
Michael Rondon <mrondon72@gmail.com>
2024-11-11 19:46:36 +0100

    420. Usar componentes de Angular Material

--- 14-my-dashboard/src/app/dashboard/pages/material/material.component.ts
+++ 14-my-dashboard/src/app/dashboard/pages/material/material.component.ts
@@ -1,8 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     standalone: true,
-    imports: [],
+    imports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSlideToggleModule
+    ],
     templateUrl: './material.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
