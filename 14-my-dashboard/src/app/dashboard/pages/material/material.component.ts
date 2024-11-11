bb14d7546b6e0cc0529b2201ff61913e800fb51c
Michael Rondon <mrondon72@gmail.com>
2024-11-11 19:58:31 +0100

    Material Bottom Sheet

--- 14-my-dashboard/src/app/dashboard/pages/material/material.component.ts
+++ 14-my-dashboard/src/app/dashboard/pages/material/material.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheet, MatBottomSheetModule, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+// Components
+import { OptionsBottomSheetComponent } from './ui/options-bottom-sheet/options-bottom-sheet.component';
 
 @Component({
     standalone: true,
     imports: [
         MatBadgeModule,
+        MatBottomSheetModule,
         MatButtonModule,
         MatIconModule,
         MatSlideToggleModule
@@ -16,5 +21,13 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class MaterialComponent {
+
+    constructor(
+        private matBottomSheet: MatBottomSheet
+    ) { }
+
+    openBottomSheet(): void {
+        this.matBottomSheet.open(OptionsBottomSheetComponent);
+    }
 
 }
