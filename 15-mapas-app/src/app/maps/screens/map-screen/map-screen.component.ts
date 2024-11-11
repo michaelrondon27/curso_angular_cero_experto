40854a3a9841a3caeb8d32a2b47be21c1252f016
Michael Rondon <mrondon72@gmail.com>
2024-11-11 20:58:09 +0100

    433. Obtener la geolocalización del usuario

--- 15-mapas-app/src/app/maps/screens/map-screen/map-screen.component.ts
+++ 15-mapas-app/src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+// Services
+import { PlacesService } from '../../services';
+
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
   styleUrl: './map-screen.component.css'
 })
 export class MapScreenComponent {
+
+    constructor(
+        private placesService: PlacesService
+    ) { }
 
 }
--- 15-mapas-app/src/main.ts
+++ 15-mapas-app/src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (!navigator.geolocation) {
+    alert('Navegador no soporta la Geolocation');
+    throw new Error('Navegador no soporta la Geolocation');
+}
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
