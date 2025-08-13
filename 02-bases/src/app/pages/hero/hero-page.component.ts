8c772f5fa202a0449f74d2e48a64e6eeed1f815a
Michael Rondon <mrondon72@gmail.com>
2025-08-13 22:59:42 +0200

    42. Señales computadas - Readonly Signals

--- 02-bases/src/app/pages/hero/hero-page.component.ts
+++ 02-bases/src/app/pages/hero/hero-page.component.ts
@@ -1,12 +1,18 @@
-import { Component, signal, WritableSignal } from "@angular/core";
+import { Component, computed, Signal, signal, WritableSignal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
 
 @Component({
+    imports: [
+        UpperCasePipe
+    ],
     templateUrl: './hero-page.component.html'
 })
 export class HeroPageComponent {
 
     public age : WritableSignal<number> = signal<number>(45);
     public name: WritableSignal<string> = signal<string>('Ironman');
+
+    public heroDescription: Signal<string> = computed<string>(() => `${ this.name() } - ${ this.age() }`);
 
     changeAge(): void {
         this.age.set(60);
@@ -15,10 +21,6 @@ export class HeroPageComponent {
     changeHero(): void {
         this.name.set('Spiderman');
         this.age.set(22);
-    }
-
-    getHeroDescription(): string {
-        return `${ this.name() } - ${ this.age() }`
     }
 
     resetForm(): void {
