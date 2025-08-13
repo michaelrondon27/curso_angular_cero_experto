516b3ed2c60e0ca7a81008d68549795d7af5e9aa
Michael Rondon <mrondon72@gmail.com>
2025-08-13 22:20:24 +0200

    37. Tarea - Separar template de la lógica

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template: `
-        <h1>Counter: {{ counter }}</h1>
-        <button (click)="increaseBy(1)">+1</button>
+    templateUrl: './counter-page.component.html',
+    styles: `
+        button {
+            margin: 5px 10px;
+            padding: 5px;
+            width: 75px;
+        }
     `
 })
 export class CounterPageComponent {
 
-    public counter: number = 15;
+    public counter: number = 10;
 
     increaseBy(value: number): void {
         this.counter += value;
+    }
+
+    resetCounter(): void {
+        this.counter = 10;
     }
 
 }
