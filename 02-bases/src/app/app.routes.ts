08cb641b1868d41477b76380a7ff251070fe5ee9
Michael Rondon <mrondon72@gmail.com>
2025-08-19 23:08:44 +0200

    50. Tarea - Preparación de página

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 
 // Components
 import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-pagecomponent';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: CounterPageComponent
+    },
+    {
+        path: 'dragonball',
+        component: DragonballPageComponent
     },
     {
         path: 'hero',
