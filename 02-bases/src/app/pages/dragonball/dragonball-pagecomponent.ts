e6cf72bd935cc0750eeb15d16e8522c38e8d6dc6
Michael Rondon <mrondon72@gmail.com>
2025-08-19 23:17:58 +0200

    51. @for - Realizar iteraciones de elementos

--- 02-bases/src/app/pages/dragonball/dragonball-pagecomponent.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-pagecomponent.ts
@@ -1,6 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, signal, WritableSignal } from "@angular/core";
+
+interface Character {
+    id   : number;
+    name : string;
+    power: number;
+}
 
 @Component({
     templateUrl: './dragonball-page.component.html'
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+
+    public characters: WritableSignal<Character[]> = signal<Character[]>([
+        { id: 1, name: 'Goku', power: 9001 },
+        { id: 2, name: 'Vegeta', power: 8000 },
+        { id: 3, name: 'Piccolo', power: 3000 }
+    ]);
+
+}
