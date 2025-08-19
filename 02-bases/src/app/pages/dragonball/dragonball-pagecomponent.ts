d7209fa069d18007ba32cde5ae143ada72ff4cdd
Michael Rondon <mrondon72@gmail.com>
2025-08-19 23:29:21 +0200

    53. @if - Mostrar elementos de forma condicional

--- 02-bases/src/app/pages/dragonball/dragonball-pagecomponent.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-pagecomponent.ts
@@ -14,7 +14,8 @@ export class DragonballPageComponent {
     public characters: WritableSignal<Character[]> = signal<Character[]>([
         { id: 1, name: 'Goku', power: 9001 },
         { id: 2, name: 'Vegeta', power: 8000 },
-        { id: 3, name: 'Piccolo', power: 3000 }
+        { id: 3, name: 'Piccolo', power: 3000 },
+        { id: 4, name: 'Yamcha', power: 500 }
     ]);
 
 }
