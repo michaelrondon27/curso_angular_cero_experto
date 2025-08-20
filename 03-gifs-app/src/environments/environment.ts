0d408b3d2aceae8c4cdd6685e2816a8dd8b1b223
Michael Rondon <mrondon72@gmail.com>
2025-08-20 22:34:14 +0200

    81. Giphy API - Servicio de Gifs

--- 03-gifs-app/src/environments/environment.ts
+++ 03-gifs-app/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     companyName  : 'Gifs',
     companyName2 : 'App',
     companySlogan: 'Maneja tus gifs',
-    production   : true,
+    giphyApiKey  : 'Jzl0BOrsPq2hUbvhpnqqG8hAevs1Ipw0',
+    production   : true
 };
