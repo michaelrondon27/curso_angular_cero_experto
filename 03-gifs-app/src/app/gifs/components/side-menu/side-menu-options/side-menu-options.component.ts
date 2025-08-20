fabdc5af7b9386025cc2f36b43d0971ff9595222
Michael Rondon <mrondon72@gmail.com>
2025-08-20 21:46:09 +0200

    72. RouterLinks - Desde componentes hijos

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+    icon    : string;
+    label   : string;
+    route   : string;
+    subLabel: string;
+}
 
 @Component({
     selector: 'gifs-side-menu-options',
-    imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
     templateUrl: './side-menu-options.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+    public menuOptions: WritableSignal<MenuOptions[]> = signal<MenuOptions[]>([
+        {
+            icon: 'fa-solid fa-chart-line',
+            label: 'Trending',
+            route: '/dashboard/trending',
+            subLabel: 'Gifs populares'
+        },
+        {
+            icon: 'fa-solid fa-magnifying-glass',
+            label: 'Buscador',
+            route: '/dashboard/search',
+            subLabel: 'Buscar gifs'
+        }
+    ]);
+
+}
