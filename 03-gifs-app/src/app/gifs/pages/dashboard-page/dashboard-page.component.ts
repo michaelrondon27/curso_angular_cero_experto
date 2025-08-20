4bad21d94f95d6762637836281ce3f6ceb4f3c8c
Michael Rondon <mrondon72@gmail.com>
2025-08-20 21:34:44 +0200

    71. Componentes para el menú lateral

--- 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Components
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
+
 @Component({
     selector: 'app-dashboard-page',
     imports: [
+        SideMenuComponent,
         RouterOutlet
     ],
     templateUrl: './dashboard-page.component.html',
