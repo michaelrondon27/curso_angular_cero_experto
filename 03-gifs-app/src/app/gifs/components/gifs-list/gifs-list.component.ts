3d2572d25fbba2a7bd42ce3b12bb5750bad565e0
Michael Rondon <mrondon72@gmail.com>
2025-08-20 22:12:20 +0200

    74. Mostrar imágenes de relleno

---
+++ 03-gifs-app/src/app/gifs/components/gifs-list/gifs-list.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+// Components
+import { GifsListItemComponent } from './gifs-list-item/gifs-list-item.component';
+
+@Component({
+    selector: 'gifs-list',
+    imports: [
+        GifsListItemComponent
+    ],
+    templateUrl: './gifs-list.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GifsListComponent { }
--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,8 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+// Components
+import { GifsListComponent } from '../../components/gifs-list/gifs-list.component';
+
 @Component({
     selector: 'app-trending-page',
-    imports: [],
+    imports: [
+        GifsListComponent
+    ],
     templateUrl: './trending-page.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
