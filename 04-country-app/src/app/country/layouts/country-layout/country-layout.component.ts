6c797acfa18781231b30ce135c222196122d491a
Michael Rondon <mrondon72@gmail.com>
2025-08-26 00:31:20 +0200

    110. Menú superior a íconos

--- 04-country-app/src/app/country/layouts/country-layout/country-layout.component.ts
+++ 04-country-app/src/app/country/layouts/country-layout/country-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Components
+import { TopMenuComponent } from '../../components/top-menu/top-menu.component';
+
 @Component({
     selector: 'app-country-layout',
     imports: [
-        RouterOutlet
+        RouterOutlet,
+        TopMenuComponent,
     ],
     templateUrl: './country-layout.component.html'
 })
