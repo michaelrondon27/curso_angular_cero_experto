7f70dd4829f6740aa6c004b03194b452035cdd0f
Michael Rondon <mrondon72@gmail.com>
2025-08-25 23:47:10 +0200

    106. Inicio de proyecto - CountryApp

---
+++ 04-country-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal, WritableSignal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html'
+})
+export class App {
+
+    protected readonly title: WritableSignal<string> = signal<string>('country-app');
+
+}
