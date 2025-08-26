b74636fd315c7ed9ca6208eed3d93d83801a8472
Michael Rondon <mrondon72@gmail.com>
2025-08-26 02:15:14 +0200

    123. Decimal Pipe

--- 04-country-app/src/app/country/components/country-list/country-list.component.ts
+++ 04-country-app/src/app/country/components/country-list/country-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, input, InputSignal } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 // Interfaces
 import { Country } from '../../interfaces/country.interface';
 
 @Component({
     selector: 'country-list',
-    imports: [],
+    imports: [
+        DecimalPipe
+    ],
     templateUrl: './country-list.component.html'
 })
 export class CountryListComponent {
