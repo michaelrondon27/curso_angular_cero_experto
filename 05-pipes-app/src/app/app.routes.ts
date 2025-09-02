cd541be2cf8fc46a667ff8ee7395415123ba768a
Michael Rondon <mrondon72@gmail.com>
2025-09-03 00:00:19 +0200

    149. Inicio de proyecto

---
+++ 05-pipes-app/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: 'basic',
+        loadComponent: () => import('./pages/basic-page/basic-page.component'),
+        title: 'Pipes Básicos'
+    },
+    {
+        path: 'custom',
+        loadComponent: () => import('./pages/custom-page/custom-page.component'),
+        title: 'Numbers Pipes'
+    },
+    {
+        path: 'numbers',
+        loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+        title: 'Pipes no tan comunes'
+    },
+    {
+        path: 'uncommon',
+        loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+        title: 'Pipes Personalizados'
+    },
+    {
+        path: '**',
+        redirectTo: 'basic'
+    }
+];
