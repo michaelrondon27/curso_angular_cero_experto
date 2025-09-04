1edb2ade21ac2edd9405ea175bf32b426becf2cd
Michael Rondon <mrondon72@gmail.com>
2025-09-04 19:34:38 +0200

    151. UpperCase, LowerCase y TitleCase Pipes

--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
     selector: 'app-basic-page',
-    imports: [],
+    imports: [
+        LowerCasePipe,
+        TitleCasePipe,
+        UpperCasePipe
+    ],
     templateUrl: './basic-page.component.html'
 })
-export default class BasicPageComponent {}
+export default class BasicPageComponent {
+
+    public fullName : WritableSignal<string> = signal<string>('mIcHaEl RoNdOn');
+    public nameLower: WritableSignal<string> = signal<string>('michael');
+    public nameUpper: WritableSignal<string> = signal<string>('MICHAEL');
+
+}
