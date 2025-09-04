d432ca36d174ccd0899ed1f45822ad885e3c7527
Michael Rondon <mrondon72@gmail.com>
2025-09-04 20:03:44 +0200

    153. Date Pipe

--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal, WritableSignal } from '@angular/core';
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, EffectRef, signal, WritableSignal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
     selector: 'app-basic-page',
     imports: [
+        DatePipe,
         LowerCasePipe,
         TitleCasePipe,
         UpperCasePipe
@@ -12,8 +13,19 @@ import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 })
 export default class BasicPageComponent {
 
-    public fullName : WritableSignal<string> = signal<string>('mIcHaEl RoNdOn');
-    public nameLower: WritableSignal<string> = signal<string>('michael');
-    public nameUpper: WritableSignal<string> = signal<string>('MICHAEL');
+    public customDate: WritableSignal<Date> = signal<Date>(new Date());
+    public fullName  : WritableSignal<string> = signal<string>('mIcHaEl RoNdOn');
+    public nameLower : WritableSignal<string> = signal<string>('michael');
+    public nameUpper : WritableSignal<string> = signal<string>('MICHAEL');
+
+    public tickingDateEffect: EffectRef = effect((onCleanup) => {
+        const interval: number = setInterval(() => {
+            this.customDate.set(new Date());
+        }, 1000);
+
+        onCleanup(() => {
+            clearInterval(interval);
+        });
+    });
 
 }
