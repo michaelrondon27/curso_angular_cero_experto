66751d79678e3829953ad4fd690de02df4ce9b20
Michael Rondon <mrondon72@gmail.com>
2025-09-04 20:15:11 +0200

    154. Cambiar de idiomas

--- 05-pipes-app/src/app/app.config.ts
+++ 05-pipes-app/src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideRouter(routes),
-        provideZoneChangeDetection({ eventCoalescing: true })
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        {
+            provide: LOCALE_ID,
+            useValue: 'es'
+        }
     ]
 };
