a78611ee5a9816bd7f53ca5de8383c594126a960
Michael Rondon <mrondon72@gmail.com>
2025-09-04 20:50:46 +0200

    157. i18nSelectPipe

--- 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { I18nSelectPipe } from '@angular/common';
 
 // Components
 import { CardComponent } from '../../components/card/card.component';
 
+interface Client {
+    address: string;
+    age    : number;
+    gender : string;
+    name   : string;
+}
+
+const client1: Client = {
+    address: 'Madrid, Spain',
+    age: 32,
+    gender: 'male',
+    name: 'Michael'
+};
+
+const client2: Client = {
+    address: 'Wroclaw, Poland',
+    age: 25,
+    gender: 'female',
+    name: 'Ania'
+};
+
 @Component({
     selector: 'app-uncommon-page',
     imports: [
-        CardComponent
+        CardComponent,
+        I18nSelectPipe
     ],
     templateUrl: './uncommon-page.component.html'
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+    public client: WritableSignal<Client> = signal<Client>(client1);
+    
+    public invitationMap = {
+        female: 'invitarla',
+        male: 'invitarlo'
+    };
+
+    changeClient(): void {
+        if (this.client() === client1) {
+            this.client.set(client2);
+            
+            return;
+        }
+
+        this.client.set(client1);
+    }
+
+}
