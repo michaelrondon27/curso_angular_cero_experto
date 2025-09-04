c68ee42fe3d56334582c7522694dbd8dab473b9c
Michael Rondon <mrondon72@gmail.com>
2025-09-04 22:10:31 +0200

    169. Pipe personalizado - ToggleCase

--- 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+
+// Pipes
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 
 @Component({
     selector: 'app-custom-page',
-    imports: [],
+    imports: [
+        ToggleCasePipe
+    ],
     templateUrl: './custom-page.component.html'
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+
+    public name     : WritableSignal<string> = signal<string>('michael rondón');
+    public upperCase: WritableSignal<boolean> = signal<boolean>(true);
+
+}
