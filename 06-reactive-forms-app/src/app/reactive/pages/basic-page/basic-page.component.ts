ce42f063fc576f2e61b42a16a27417b7c6a650c2
Michael Rondon <mrondon72@gmail.com>
2025-09-20 23:31:49 +0200

    181. ReactiveForms - FormGroup y FormControl

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-basic-page',
     imports: [
-        JsonPipe
+        JsonPipe,
+        ReactiveFormsModule
     ],
     templateUrl: './basic-page.component.html'
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+
+    public myForm: WritableSignal<FormGroup> = signal<FormGroup>(new FormGroup({
+        inStorage: new FormControl(0),
+        name: new FormControl(''),
+        price: new FormControl(0)
+    }))
+
+}
