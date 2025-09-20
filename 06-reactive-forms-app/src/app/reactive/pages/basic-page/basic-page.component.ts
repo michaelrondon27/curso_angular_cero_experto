643dd30ad501cd93d45d9f8df40d4cd6e02eae5b
Michael Rondon <mrondon72@gmail.com>
2025-09-20 23:47:52 +0200

    183. Validaciones de campos incluidos

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-basic-page',
@@ -15,9 +15,9 @@ export default class BasicPageComponent {
     private _formBuilder: FormBuilder = inject(FormBuilder);
 
     public myForm: WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
-        inStorage: [0],
-        name: [''],
-        price: [0]
+        inStorage: [0, [Validators.min(0), Validators.required]],
+        name: ['', [Validators.minLength(3), Validators.required]],
+        price: [0, [Validators.min(10), Validators.required]]
     }));
 
 }
