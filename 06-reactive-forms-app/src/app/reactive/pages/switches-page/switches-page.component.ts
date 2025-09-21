2f41cf356d92d8886050fc46b7f625bd20c00bc9
Michael Rondon <mrondon72@gmail.com>
2025-09-21 23:26:59 +0200

    196. Switches, Checks y Radio buttons

--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// Utils
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
     selector: 'app-switches-page',
     imports: [
-        JsonPipe
+        JsonPipe,
+        ReactiveFormsModule
     ],
     templateUrl: './switches-page.component.html'
 })
-export default class SwitchesPageComponent { }
+export default class SwitchesPageComponent {
+
+    private _formBuilder: FormBuilder = inject(FormBuilder);
+
+    public formUtils: WritableSignal<typeof FormUtils> = signal<typeof FormUtils>(FormUtils);
+    public myForm   : WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
+        gender: ['M', [Validators.required]],
+        termAndConditions: [false, [Validators.requiredTrue]],
+        wantNotifications: [true]
+    }));
+
+    onSubmit(): void {
+        this.myForm().markAllAsTouched();
+
+        console.log(this.myForm().value)
+    }
+
+}
