9c14f34a22704dfd43c06311926c8e565c1d54e0
Michael Rondon <mrondon72@gmail.com>
2025-09-21 20:56:48 +0200

    189. Agregar y eliminar controles de formularios

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 // Utils
 import { FormUtils } from '../../../utils/form-utils';
@@ -17,8 +17,8 @@ export default class DynamicPageComponent {
 
     private _formBuilder: FormBuilder = inject(FormBuilder);
 
-    public formUtils: WritableSignal<typeof FormUtils> = signal<typeof FormUtils>(FormUtils);
-    public myForm   : WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
+    public formUtils  : WritableSignal<typeof FormUtils> = signal<typeof FormUtils>(FormUtils);
+    public myForm     : WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
         favoriteGames: this._formBuilder.array(
             [
                 ['Metal Gear', [Validators.required]],
@@ -28,9 +28,30 @@ export default class DynamicPageComponent {
         ),
         name: ['', [Validators.minLength(3), Validators.required]]
     }));
+    public newFavorite: WritableSignal<FormControl> = signal<FormControl>(new FormControl('', [Validators.required]));
 
     get favoriteGames(): FormArray {
         return this.myForm().get('favoriteGames') as FormArray;
+    }
+
+    onAddToFavorites(): void {
+        if (this.newFavorite().invalid ) {
+            return;
+        }
+
+        const newGame: string = this.newFavorite().value;
+
+        this.favoriteGames.push(this._formBuilder.control(newGame, [Validators.required]));
+
+        this.newFavorite().reset();
+    }
+
+    onDeleteFavorite(index: number): void {
+        this.favoriteGames.removeAt(index);
+    }
+
+    onSubmit(): void {
+        this.myForm().markAllAsTouched();
     }
 
 }
