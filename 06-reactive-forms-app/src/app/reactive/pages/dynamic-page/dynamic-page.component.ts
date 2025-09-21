ac094cd0bd294db3b9b9d1c68cca8507eb5b5c61
Michael Rondon <mrondon72@gmail.com>
2025-09-21 17:26:19 +0200

    187. Formularios dinámicos con arreglos

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-dynamic-page',
     imports: [
-        JsonPipe
+        JsonPipe,
+        ReactiveFormsModule
     ],
     templateUrl: './dynamic-page.component.html'
 })
-export default class DynamicPageComponent { }
+export default class DynamicPageComponent {
+
+    private _formBuilder: FormBuilder = inject(FormBuilder);
+
+    public myForm: WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
+        favoriteGames: this._formBuilder.array(
+            [
+                ['Metal Gear', [Validators.required]],
+                ['Death Stranding', [Validators.required]]
+            ],
+            [Validators.minLength(3)]
+        ),
+        name: ['', [Validators.minLength(3), Validators.required]]
+    }));
+
+    get favoriteGames(): FormArray {
+        return this.myForm().get('favoriteGames') as FormArray;
+    }
+
+}
