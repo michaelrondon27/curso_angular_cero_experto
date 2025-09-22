7bddba03dfae109ff0bcaeb3011fc0d78d86190d
Michael Rondon <mrondon72@gmail.com>
2025-09-22 21:22:44 +0200

    203. Primer selector controlado - Continentes

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+// Services
+import { CountryService } from '../../services/country.service';
 
 @Component({
     selector: 'app-country-page',
-    imports: [],
+    imports: [
+        JsonPipe,
+        ReactiveFormsModule
+    ],
     templateUrl: './country-page.component.html'
 })
-export default class CountryPageComponent { }
+export default class CountryPageComponent {
+
+    private _countryService: CountryService = inject(CountryService);
+    private _formBuilder   : FormBuilder = inject(FormBuilder);
+
+    public borders  : WritableSignal<Country[]> = signal<Country[]>([]);
+    public countries: WritableSignal<Country[]> = signal<Country[]>([]);
+    public myForm   : WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
+        border: ['', [Validators.required]],
+        country: ['', [Validators.required]],
+        region: ['', [Validators.required]]
+    }));
+    public regions  : WritableSignal<string[]> = signal<string[]>(this._countryService.regions);
+
+}
