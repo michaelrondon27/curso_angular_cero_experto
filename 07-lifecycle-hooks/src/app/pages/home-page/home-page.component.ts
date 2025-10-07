a100c406f0606247d2852d6fb80df1e79f7a1440
Michael Rondon <mrondon72@gmail.com>
2025-10-07 16:56:01 +0200

    213. Ciclo de vida - Segunda Parte

--- 07-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
+++ 07-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, afterEveryRender, afterNextRender, AfterViewChecked, AfterViewInit, Component, DoCheck, effect, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 const log = (...messages: string[]) => {
     console.log(
@@ -12,38 +12,58 @@ const log = (...messages: string[]) => {
     imports: [],
     templateUrl: './home-page.component.html'
 })
-export default class HomePageComponent implements AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnChanges, OnInit {
+export default class HomePageComponent implements AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnChanges, OnDestroy, OnInit {
 
     constructor() {
-        log('Constructor llamado');
+        log("Constructor llamado.");
     }
 
+    basicEffect = effect((onCleanup) => {
+        log("effect", "Disparar efectos secundarios.");
+
+        onCleanup(() => {
+            log("onCleanup", "Se ejecuta cuando el efecto se va a destruir.");
+        })
+    });
+
     ngOnInit(): void {
-        log("ngOnInit", "Runs once after Angular has initialized all the component's inputs");
+        log("ngOnInit", "Runs once after Angular has initialized all the component's inputs.");
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        log("ngOnChanges", "Runs every time the component's have changed");
+        log("ngOnChanges", "Runs every time the component's have changed.");
     }
 
     ngDoCheck(): void {
-        log("ngDoCheck", "Runs every time this component is checked for changes");
+        log("ngDoCheck", "Runs every time this component is checked for changes.");
     }
 
     ngAfterContentInit(): void {
-        log("ngAfterContentInit", "Runs once after the component's content has been initialized");
+        log("ngAfterContentInit", "Runs once after the component's content has been initialized.");
     }
 
     ngAfterContentChecked(): void {
-        log("ngAfterContentChecked", "Runs every time this component's content has been checked for changes");
+        log("ngAfterContentChecked", "Runs every time this component content has been checked for changes.");
     }
 
     ngAfterViewInit(): void {
-        log("ngAfterViewInit", "Runs once after the component's conteviewnt has been initialized");
+        log("ngAfterViewInit", "Runs once after the component's conteviewnt has been initialized.");
     }
 
     ngAfterViewChecked(): void {
-        log("ngAfterViewInit", "Runs every time this component's view has been checked for changes");
+        log("ngAfterViewChecked", "Runs every time this component's view has been checked for changes.");
     }
+
+    ngOnDestroy(): void {
+        log("ngOnDestroy", "Runs once before the component is destroyed.");
+    }
+
+    afterNextRenderEffect = afterNextRender(() => {
+        log("afterNextRender", "Runs once the next time that all components have been rendered to the DOM.");
+    });
+
+    afterEveryRenderEffect = afterEveryRender(() => {
+        log("afterEveryRender", "Runs every time all components have been rendered to the DOM.");
+    });
 
 }
