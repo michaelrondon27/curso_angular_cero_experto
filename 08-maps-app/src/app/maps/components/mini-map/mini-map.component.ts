01023a18e88d7b1031f5dfffe5c91d655eae4f79
Michael Rondon <mrondon72@gmail.com>
2025-11-07 01:26:50 +0100

    235. Solución de la tarea - MiniMap

--- 08-maps-app/src/app/maps/components/mini-map/mini-map.component.ts
+++ 08-maps-app/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, input, InputSignal, Signal, viewChild } from '@angular/core';
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
+
+// Environments
+import { environment } from '../../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
     selector: 'app-mini-map',
     imports: [],
-    templateUrl: './mini-map.component.html'
+    templateUrl: './mini-map.component.html',
+    styles: `
+        div {
+            height: 260px;
+            width: 100%;
+        }
+    `
 })
-export class MiniMapComponent { }
+export class MiniMapComponent implements AfterViewInit {
+
+    public lngLat: InputSignal<LngLatLike> = input.required<LngLatLike>();
+    public zoom  : InputSignal<number> = input<number>(15);
+
+    public divElement: Signal<ElementRef | undefined> = viewChild<ElementRef>('map');
+
+    async ngAfterViewInit(): Promise<void> {
+        if (!this.divElement()?.nativeElement) {
+            return;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, 80));
+
+        const element = this.divElement()!.nativeElement;
+
+        const map = new mapboxgl.Map({
+            center: this.lngLat(),
+            container: element,
+            interactive: false,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            zoom: this.zoom()
+        });
+
+        new mapboxgl.Marker().setLngLat(this.lngLat()).addTo(map);
+    }
+
+}
--- 08-maps-app/src/app/pages/houses-page/houses-page.component.ts
+++ 08-maps-app/src/app/pages/houses-page/houses-page.component.ts
@@ -26,7 +26,7 @@ export default class HousesPageComponent {
         {
             description: 'Un refugio tranquilo con vistas panorámicas al mar y jardines exuberantes.',
             id: uuid(),
-            lngLat: { lng: -0.861526, lat: 41.65649 },
+            lngLat: { lng: -122.3893, lat: 37.7785 },
             name: 'Villa Serenidad',
             price: 500_000,
             tags: ['Villa', 'Mar', 'Jardines']
@@ -34,7 +34,7 @@ export default class HousesPageComponent {
         {
             description: 'Una casa luminosa y acogedora con amplias terrazas y piscina privada.',
             id: uuid(),
-            lngLat: { lng: -0.862, lat: 41.657 },
+            lngLat: { lng: -118.2400, lat: 34.0738 },
             name: 'Casa del Sol',
             price: 750_000,
             tags: ['Casa', 'Sol', 'Terrazas']
@@ -42,7 +42,7 @@ export default class HousesPageComponent {
         {
             description: 'Elegante propiedad con acabados de lujo y un diseño arquitectónico moderno.',
             id: uuid(),
-            lngLat: { lng: -0.863, lat: 41.658 },
+            lngLat: { lng: -117.8828, lat: 33.8002 },
             name: 'Residencia Esmeralda',
             price: 1_200_000,
             tags: ['Casa', 'Esmeralda', 'Acabados']
@@ -50,7 +50,7 @@ export default class HousesPageComponent {
         {
             description: 'Encantadora hacienda con acceso directo al lago y un entorno natural impresionante.',
             id: uuid(),
-            lngLat: { lng: -0.864, lat: 41.659 },
+            lngLat: { lng: -117.1572, lat: 32.7075 },
             name: 'Hacienda del Lago',
             price: 950_000,
             tags: ['Casa', 'Lago', 'Hacienda'],
