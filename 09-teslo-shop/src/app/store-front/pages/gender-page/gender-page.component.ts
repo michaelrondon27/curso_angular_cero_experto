aabdca8094fd733ff622fe59773e67b5385d5db6
Michael Rondon <mrondon72@gmail.com>
2025-11-12 19:42:15 +0100

    253. Página de productos por género

--- 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, ResourceRef, Signal } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
+// Components
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+
+// Interfaces
+import { ProductsResponse } from '@products/interfaces/product.interface';
+
+// Services
+import { ProductsService } from '@products/services/products.service';
 @Component({
     selector: 'app-gender-page',
-    imports: [],
+    imports: [
+        ProductCardComponent
+    ],
     templateUrl: './gender-page.component.html'
 })
-export default class GenderPageComponent { }
+export default class GenderPageComponent {
+
+    private _activatedRoute : ActivatedRoute = inject(ActivatedRoute);
+    private _productsService: ProductsService = inject(ProductsService);
+
+    public gender: Signal<string | undefined> = toSignal<string>(this._activatedRoute.params.pipe(
+        map(({ gender }) => gender)
+    ));
+
+
+    public productsResource: ResourceRef<ProductsResponse | undefined> = rxResource({
+        params: () => ({ gender: this.gender() }),
+        stream: ({ params }) => {
+            return this._productsService.getProducts({ gender: params.gender });
+        }
+    });
+
+}
