f842733ea25d698d51cd193bf70e401ddd8139ca
Michael Rondon <mrondon72@gmail.com>
2025-11-18 19:27:13 +0100

    271. Formulario reactivo de autenticación

--- 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
+++ 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-login-page',
     imports: [
+        ReactiveFormsModule,
         RouterLink
     ],
     templateUrl: './login-page.component.html'
 })
-export default class LoginPageComponent { }
+export default class LoginPageComponent {
+
+    private _formBuilder: FormBuilder = inject(FormBuilder);
+
+    public hasError : WritableSignal<boolean> = signal<boolean>(false);
+    public isPosting: WritableSignal<boolean> = signal<boolean>(false);
+    public loginForm: WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
+        email: ['', [Validators.email, Validators.required]],
+        password: ['', [Validators.minLength(6), Validators.required]]
+    }));
+
+    onSubmit(): void {
+        if (this.loginForm().invalid) {
+            this.hasError.set(true);
+
+            setTimeout(() => {
+                this.hasError.set(false);
+            }, 2000);
+
+            return;
+        }
+
+        const { email = '', password = '' } = this.loginForm().value;
+
+        console.log({ email, password });
+    }
+
+}
