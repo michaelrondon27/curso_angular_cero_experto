f5b8c811b7d4dab8a0bf542cc8302a62ab9bd8a1
Michael Rondon <mrondon72@gmail.com>
2025-11-18 20:25:34 +0100

    277. Mostrar usuario autenticado y cerrar sesión

--- 09-teslo-shop/src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ 09-teslo-shop/src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+
+// Interfaces
+import { User } from '@auth/interfaces/user.interface';
+
+// Services
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
     selector: 'front-navbar',
@@ -9,4 +15,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     ],
     templateUrl: './front-navbar.component.html'
 })
-export class FrontNavbarComponent { }
+export class FrontNavbarComponent {
+
+    private _authService: AuthService = inject(AuthService);
+
+    public authStatus: Signal<string> = computed<string>(() => this._authService.authStatus());
+    public user      : Signal<User | null> = computed<User | null>(() => this._authService.user());
+
+    logout(): void {
+        this._authService.logout();
+    }
+
+}
