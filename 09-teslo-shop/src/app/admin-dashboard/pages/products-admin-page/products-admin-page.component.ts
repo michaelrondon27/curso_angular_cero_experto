17a763d7d736c783323e102483b439998fb26153
Michael Rondon <mrondon72@gmail.com>
2026-01-27 23:55:54 +0100

    290. Mostrar productos paginados

--- 09-teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ 09-teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, linkedSignal, ResourceRef, signal, WritableSignal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 // Components
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
 import { ProductsTableComponent } from '@products/components/products-table/products-table.component';
+
+// Interfaces
+import { ProductsResponse } from '@products/interfaces/product.interface';
+
+// Services
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
     selector: 'app-products-admin-page',
     imports: [
+        PaginationComponent,
         ProductsTableComponent
     ],
     templateUrl: './products-admin-page.component.html'
 })
-export default class ProductsAdminPageComponent { }
+export default class ProductsAdminPageComponent {
+
+    private _paginationService: PaginationService = inject(PaginationService);
+    private _productsService: ProductsService = inject(ProductsService);
+
+    public productsPerPage: WritableSignal<number> = signal<number>(10);
+
+    public currentPage: WritableSignal<number> = linkedSignal<number>(
+        this._paginationService.currentPage
+    );
+
+    public productsResource: ResourceRef<ProductsResponse | undefined> = rxResource({
+        params: () => ({
+            limit: this.productsPerPage(),
+            page: this.currentPage() - 1
+        }),
+        stream: ({ params }) => {
+            return this._productsService.getProducts({
+                limit: params.limit,
+                offset: params.page * params.limit
+            });
+        }
+    });
+
+}
