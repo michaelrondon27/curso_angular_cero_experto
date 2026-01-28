bf850d8a60f6728d1d827803d32bef655312f34e
Michael Rondon <mrondon72@gmail.com>
2026-01-28 20:14:53 +0100

    291. Página de producto por ID

--- 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, ResourceRef, Signal } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+
+// Interfaces
+import { Product } from '@products/interfaces/product.interface';
+
+// Services
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
     selector: 'app-product-admin-page',
     imports: [],
     templateUrl: './product-admin-page.component.html'
 })
-export default class ProductAdminPageComponent { }
+export default class ProductAdminPageComponent {
+
+    private _activatedRoute : ActivatedRoute = inject(ActivatedRoute);
+    private _productsService: ProductsService = inject(ProductsService);
+    private _router         : Router = inject(Router);
+
+    public productId: Signal<string| undefined> = toSignal<string | undefined>(this._activatedRoute.params.pipe(
+        map((params => params['id']))
+    ));
+
+    public productResource: ResourceRef<Product | undefined> = rxResource({
+        params: () => ({ id: this.productId() }),
+        stream: ({ params }) => {
+            return this._productsService.getProductById(params.id!)
+        }
+    });
+
+    redirectEffect = effect(() =>  {
+        if (this.productResource.error()) {
+            this._router.navigate(['/admin/products']);
+        }
+    });
+
+}
--- 09-teslo-shop/src/app/products/services/products.service.ts
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -24,6 +24,16 @@ export class ProductsService {
     private _productCache: Map<string, Product> = new Map<string, Product>();
     private _productsCache: Map<string, ProductsResponse> = new Map<string, ProductsResponse>();
 
+    getProductById(id: string): Observable<Product> {
+        if (this._productCache.has(id)) {
+            return of(this._productCache.get(id)!);
+        }
+
+        return this._httpClient.get<Product>(`${ baseUrl }/products/${ id }`).pipe(
+            tap((resp: Product) => this._productCache.set(id, resp))
+        );
+    }
+
     getProductByIdSlug(idSlug: string): Observable<Product> {
         if (this._productCache.has(idSlug)) {
             return of(this._productCache.get(idSlug)!);
