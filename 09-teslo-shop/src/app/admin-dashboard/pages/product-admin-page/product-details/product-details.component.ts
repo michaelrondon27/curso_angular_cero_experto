60267a221658d74e6681d49689bae80eb19a4051
Michael Rondon <mrondon72@gmail.com>
2026-01-28 20:53:10 +0100

    293. Formulario de producto

--- 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, input, InputSignal, signal, WritableSignal } from '@angular/core';
+import { Component, inject, input, InputSignal, OnInit, signal, WritableSignal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 // Components
 import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
@@ -6,17 +7,47 @@ import { ProductCarouselComponent } from '@products/components/product-carousel/
 // Interfaces
 import { Product } from '@products/interfaces/product.interface';
 
+// Utils
+import { FormUtils } from '@utils/form-utils';
+
 @Component({
     selector: 'product-details',
     imports: [
-        ProductCarouselComponent
+        ProductCarouselComponent,
+        ReactiveFormsModule
     ],
     templateUrl: './product-details.component.html'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 
-    public sizes: WritableSignal<string[]> = signal<string[]>(['XS', 'S', 'M', 'L', 'XL', 'XXL']);
+    private _formBuilder: FormBuilder = inject(FormBuilder);
+
+    public productForm: WritableSignal<FormGroup> = signal<FormGroup>(this._formBuilder.group({
+        description: ['', [Validators.required]],
+        gender: ['men', [Validators.pattern(/men|women|kid|unisex/), Validators.required]],
+        images: [[]],
+        price: [0, [Validators.min(0), Validators.required]],
+        sizes: [['']],
+        slug: ['', [Validators.pattern(FormUtils.slugPattern()), Validators.required]],
+        stock: [0, [Validators.min(0), Validators.required]],
+        tags: [''],
+        title: ['', [Validators.required]]
+    }));
+    public sizes      : WritableSignal<string[]> = signal<string[]>(['XS', 'S', 'M', 'L', 'XL', 'XXL']);
 
     public product: InputSignal<Product> = input.required<Product>();
+
+    ngOnInit(): void {
+        this._setFormValue(this.product());
+    }
+
+    onSubmit(): void {
+        console.log(this.productForm().value);
+    }
+
+    private _setFormValue(formLike: Partial<Product>) {
+        this.productForm().reset(formLike);
+        this.productForm().patchValue({ tags: formLike.tags?.join(',') });
+    }
 
 }
